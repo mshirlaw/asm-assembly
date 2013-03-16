68114a889457f59d8c60727d0c8439417fb1651d
Matt Shirlaw <mshirlaw@yahoo.com>
2013-03-16 23:20:47 +1100

    Uploading echo and read_chars

---
+++ read_chars.asm
@@ -0,0 +1,38 @@
+	;; Reads characters from stdin until <return> 
+	;; read_chars.asm
+	;; yasm -f elf -m amd64 read_chars.asm
+	;; on 64 bit architecture
+	;; 
+	;; on 32 bit architecture
+	;; yasm -f elf -m x86 read_chars.asm
+	;;
+	;; linking
+	;; ld -o read_chars read_chars.o
+	;;
+
+section .bss				; uninitialised data
+
+	mychar 	resb	1		; reserve space for 1 bytes
+	
+section .data				; initialised data and constants
+	
+	cr	equ 	0x0A		; carriage return character
+		
+section .text				; instructions
+	global _start
+
+_start:
+	
+reading:	
+	mov 	edx, 1			; length is equal to one byte
+	mov 	ecx, mychar		; the &char to be read
+	mov 	ebx, 0			; read from stdin (0)
+	mov 	eax, 3			; 3 is the "read" system call
+	int 	0x80			; make the interupt
+	
+	mov 	al, [mychar]		; move the character at &mychar to the lower half of ax 
+	cmp	al,cr			; compare with the carriage return
+	jne	reading			; repeat if not equal to return
+	
+	mov	eax, 1			; the exit call is 1	 
+	int 	0x80			; make the inteuupt
